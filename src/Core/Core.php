eb1d38d05bfdc050ec5787b248c1581c0bacb7f5
Stuart McAlpine <stu@pressingspace.com>
2024-12-02 12:43:57 +0000

    Show BeyondWords fields in Custom Fields panel while editing in Classic Editor

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -303,10 +303,20 @@ class Core
      *
      * https://github.com/WordPress/gutenberg/issues/23078
      *
-     * @since 4.0.0
+     * @since 4.0.0 Introduced.
+     * @since 5.2.2 Only apply this filter when the Block Editor is active.
+     *
+     * @param bool   $protected Whether the meta key is protected.
+     * @param string $metaKey   Meta key.
+     *
+     * @return bool
      */
     public function isProtectedMeta($protected, $metaKey)
     {
+        if (! CoreUtils::isGutenbergPage()) {
+            return $protected;
+        }
+
         $keysToProtect = CoreUtils::getPostMetaKeys('all');
 
         if (in_array($metaKey, $keysToProtect, true)) {
