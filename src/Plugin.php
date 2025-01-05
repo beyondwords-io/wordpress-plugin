1d14ce8e8c0c47591c92e28860a260231401b866
Stuart McAlpine <stu@pressingspace.com>
2025-01-05 13:47:09 +0000

    PlayerContent classic editor component

--- src/Plugin.php
+++ src/Plugin.php
@@ -17,6 +17,7 @@ use Beyondwords\Wordpress\Component\Post\ErrorNotice\ErrorNotice;
 use Beyondwords\Wordpress\Component\Post\GenerateAudio\GenerateAudio;
 use Beyondwords\Wordpress\Component\Post\Metabox\Metabox;
 use Beyondwords\Wordpress\Component\Post\Panel\Inspect\Inspect;
+use Beyondwords\Wordpress\Component\Post\PlayerContent\PlayerContent;
 use Beyondwords\Wordpress\Component\Post\PlayerStyle\PlayerStyle;
 use Beyondwords\Wordpress\Component\Post\SelectVoice\SelectVoice;
 use Beyondwords\Wordpress\Component\Posts\Column\Column;
@@ -99,6 +100,7 @@ class Plugin
             (new DisplayPlayer())->init();
             (new SelectVoice())->init();
             (new PlayerStyle())->init();
+            (new PlayerContent())->init();
             (new Metabox())->init();
         }
     }
