cc335868e8835b46ca637c34ed37ca810793488e
Stuart McAlpine <stu@pressingspace.com>
2025-01-30 20:05:27 +0000

    Create Summarization tab in WordPress Settings

---
+++ tests/cypress/e2e/settings/summarization/manage-summarization-button.cy.js
@@ -0,0 +1,19 @@
+context( 'Settings > Summarization',  () => {
+  before( () => {
+    cy.task( 'reset' )
+    cy.login()
+    cy.saveStandardPluginSettings()
+  } )
+
+  beforeEach( () => {
+    cy.login()
+  } )
+
+  it( 'has the "Manage summarization" button', () => {
+    cy.saveMinimalPluginSettings()
+
+    cy.visit( '/wp-admin/options-general.php?page=beyondwords&tab=summarization' )
+    cy.get( '.button.button-primary' ).eq( 0 ).invoke( 'text' ).then( text => text.trim() ).should( 'equal', 'Manage summarization' );
+    cy.get( '.button.button-primary' ).eq( 0 ).should( 'have.attr', 'href', `https://dash.beyondwords.io/dashboard/project/${ Cypress.env( 'projectId' ) }/settings?tab=summarization` )
+  } )
+} )
